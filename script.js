f5d8d39d8b8183e8d4d09ff517bda98cc569bee3
Chadi25 <94203613+Chadi25@users.noreply.github.com>
2024-05-02 15:02:16 +0200

    update

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-const modeSwitch = document.getElementById('mode-switch');
+const dayModeButton = document.getElementById('day-mode');
+const nightModeButton = document.getElementById('night-mode');
 const container = document.querySelector('.container');
 const body = document.body;
 const certificationTitle = document.querySelector('.certifications h2');
 
-modeSwitch.addEventListener('change', function() {
-    body.classList.toggle('dark-mode');
-    container.classList.toggle('dark-mode');
-    certificationTitle.classList.toggle('dark-mode');
-
+dayModeButton.addEventListener('click', function() {
+    body.classList.remove('dark-mode');
+    container.classList.remove('dark-mode');
+    certificationTitle.classList.remove('dark-mode');
     const socialLinks = document.querySelectorAll('.social-links a');
     socialLinks.forEach(link => {
-        link.classList.toggle('dark-mode');
+        link.classList.remove('dark-mode');
     });
+    dayModeButton.classList.add('active');
+    nightModeButton.classList.remove('active');
+});
+
+nightModeButton.addEventListener('click', function() {
+    body.classList.add('dark-mode');
+    container.classList.add('dark-mode');
+    certificationTitle.classList.add('dark-mode');
+    const socialLinks = document.querySelectorAll('.social-links a');
+    socialLinks.forEach(link => {
+        link.classList.add('dark-mode');
+    });
+    nightModeButton.classList.add('active');
+    dayModeButton.classList.remove('active');
 });
